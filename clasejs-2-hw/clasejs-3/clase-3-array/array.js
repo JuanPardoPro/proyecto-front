c3fd41b161c66bbd994d0cfd0d8fb97d9090c3ff
mauruecar64 <mruedaca8006@universidadean.edu.co>
2023-06-13 21:00:38 -0500

    arreglos

---
+++ clasejs-2-hw/clasejs-3/clase-3-array/array.js
@@ -0,0 +1,28 @@
+const friends = ('mrc', 'lfm', 'mrm');
+
+console.log (friends [2]);
+friends.length; //devuelve la cantidad deelmentos
+indexofMrc = friends.includes ('mrc'); // retorna elindice sino esta regresa -1:
+friends.push ('Anto'); // adiciona un elent al final
+console.log (friends);
+friends.pop (); // elimna el ultimo elemento d ela lista
+friends.shift (); // elimina le primerelemnto
+
+let string = ('jamon, papaya, aguacate');
+string.split (','); // convierte un string en un arreglo
+
+const newNames = friends (1,2); //crea unn uco arreglo desde un arreglo refernrciado desde la posición inicial a la final iidcada
+const newNames1 = friends (2); //crea unn nuevo arreglo desde un arreglo refernrciado desde la posicion dada hasta el final
+
+slice.friends (2,0,'paco', 'riky') // me inserta 2 elementos desde la posición dos, puede insertar o cambiar o elimianr
+
+//sort usa elmetodo utf16
+
+//reverse invierte el orden del arreglo y alera el areglo original
+
+// concat une dos arreglos en un nuevo arreglo (uno detrasd e otro según el orden dle cat)
+
+
+
+
+
